f30f25d401360e22a489822845dd869810f97648
Irrrka <irrrka@gmil.com>
2019-10-18 01:38:03 +0300

    RemoteWebDriverAdded

---
+++ Homework/Tests/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Homework.Tests
+{
+    [TestFixture]
+    public class BaseTest
+    {
+        public IWebDriver _driver { get; private set; }
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+    }
+}
