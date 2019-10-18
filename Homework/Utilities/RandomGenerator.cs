e74786bdd333df3552a04a88375c2f95d9b3c5da
Irrrka <irrrka@gmil.com>
2019-10-19 01:01:16 +0300

    GoogleSearch and QAAutomation added

---
+++ Homework/Utilities/RandomGenerator.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework.Utilities
+{
+    public static class RandomGenerator
+    {
+        public static string GenerateMail()
+        {
+            return "";
+        }
+
+    }
+}
